f4b2c80eb0c527e1d5bc4a3830e424f161c6134c
Brad Smith <bradster@infinitewarp.com>
2018-04-30 20:32:08 -0400

    add script to geotag a photo

---
+++ photos/geotag.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from pexif import JpegFile
+import sys
+
+usage = """Usage: setgps.py filename.jpg lat lng"""
+
+if len(sys.argv) != 4:
+    print(usage)
+    sys.exit(1)
+
+try:
+    ef = JpegFile.fromFile(sys.argv[1])
+    ef.set_geo(float(sys.argv[2]), float(sys.argv[3]))
+except IOError:
+    type, value, traceback = sys.exc_info()
+    print("Error opening file:", value)
+except JpegFile.InvalidFile:
+    type, value, traceback = sys.exc_info()
+    print("Error opening file:", value)
+
+try:
+    ef.writeFile(sys.argv[1])
+except IOError:
+    type, value, traceback = sys.exc_info()
+print("Error saving file:", value)
+
+
